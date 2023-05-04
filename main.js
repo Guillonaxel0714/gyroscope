95dc2a2b54e19b52e7e685eef8ad09081019781d
Axel Guillon <axel.guillon@edu.devinci.fr>
2023-05-04 09:37:09 +0200

    add

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ camera.position.z = 5;
 
 function animate() {
 
+  cube.rotation.z = degToRad(alpha) / 2;
   cube.rotation.x = degToRad(beta);
   cube.rotation.y = degToRad(gamma);
-  cube.rotation.z = degToRad(alpha);
 
   renderer.render(scene, camera);
 }

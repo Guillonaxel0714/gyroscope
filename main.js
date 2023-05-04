1af1bf5a2d7c24c220985ee86c1a8ec66cdae888
Axel Guillon <axel.guillon@edu.devinci.fr>
2023-05-04 14:41:29 +0200

    test import gltf

--- main.js
+++ main.js
@@ -1,34 +1,20 @@
 import * as THREE from 'three';
+import {
+  GLTFLoader
+} from 'three/addons/loaders/GLTFLoader.js';
 
 let alpha, beta, gamma = 0;
 
-function getOrientation () {
-  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-    DeviceOrientationEvent.requestPermission()
-      .then(permissionState => {
-        if (permissionState === 'granted') {
-          window.addEventListener('deviceorientation', (event) => {
-            alpha = event.alpha;
-            beta = event.beta;
-            gamma = event.gamma;
-          });
-        }
-      })
-      .catch(console.error);
-  } else {
-    window.addEventListener('deviceorientation', (event) => {
-      alpha = event.alpha;
-      beta = event.beta;
-      gamma = event.gamma;
-    });
-  }
-}
+window.addEventListener('deviceorientation', (event) => {
+  alpha = event.alpha;
+  beta = event.beta;
+  gamma = event.gamma;
+});
 
-getOrientation();
-
-const degToRad = (deg) =>  deg * (Math.PI / 180);
+const degToRad = (deg) => deg * (Math.PI / 280);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
@@ -37,20 +23,57 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial({
-  color: 0x00ff00
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+camera.position.z = 100;
+// Créer une lumière ambiante
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-camera.position.z = 5;
+// Créer une lumière directionnelle
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(1, 1, 1);
+scene.add(directionalLight);
+
+// Charge le modèle GLTF en utilisant GLTFLoader
+const loader = new GLTFLoader();
+loader.load(
+  '/assets/maze.gltf',
+  function (gltf) {
+
+    // Récupère l'objet principal de la scène du modèle GLTF
+    const object = gltf.scene.children[0];
+
+    // Récupère le matériau de l'objet
+    object.material = new THREE.MeshStandardMaterial({
+      color: 0xff0000 // Rouge
+    });
+
+    // Redimensionne l'objet
+    object.scale.set(0.5, 0.5, 0.5); // Par exemple, divise la taille par 2
+
+    // Incliner légèrement le mesh vers l'arrière
+    object.rotation.x = -0.99;
+    object.rotation.y = 0.5;
+
+    // Ajoute l'objet à la scène
+    scene.add(object);
+  },
+  function (xhr) {
+    // Fonction de progression
+    console.log((xhr.loaded / xhr.total * 100) + '% chargé');
+  },
+  function (error) {
+    // Fonction d'erreur
+    console.error('Erreur de chargement', error);
+  }
+);
 
 function animate() {
 
-  cube.rotation.z = degToRad(alpha) / 2;
-  cube.rotation.x = degToRad(beta);
-  cube.rotation.y = degToRad(gamma);
+  scene.rotation.z = degToRad(alpha) / 2;
+  scene.rotation.x = degToRad(beta);
+  scene.rotation.y = degToRad(gamma);
 
   renderer.render(scene, camera);
 }
+
+animate();

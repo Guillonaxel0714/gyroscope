480d9de88fb2af1dc6f46fc41241e38d8918212c
Guillonaxel0714 <axel.guillon@edu.devinci.fr>
2023-05-08 17:23:18 +0200

    sake

--- main.js
+++ main.js
@@ -75,9 +75,8 @@ loader.load(
       const acceleration = event.accelerationIncludingGravity;
 
       if (acceleration.x > 15 || acceleration.y > 15 || acceleration.z > 15) {
-        alert('You win !');
         const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        object.material.color.set('#' + randomColor);
+        maze.material.color.set('#' + randomColor);
       }
     });
   },
